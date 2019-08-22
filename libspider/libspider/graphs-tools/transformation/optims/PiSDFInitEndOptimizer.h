05438d66487926cb596b0e5667d718dbf3cced78
Arrestier Florian <florian.arrestier@insa-rennes.fr>
2019-08-22 17:35:08 +0200

    (OPTIMS) adding init end optim.

--- libspider/libspider/graphs-tools/transformation/optims/PiSDFInitEndOptimizer.h
+++ libspider/libspider/graphs-tools/transformation/optims/PiSDFInitEndOptimizer.h
@@ -52,7 +52,29 @@ public:
 };
 
 PiSDFGraph *PiSDFInitEndOptimizer::operator()(PiSDFGraph *graph) const {
-    return nullptr;
+    Spider::vector<PiSDFVertex *> verticesToOptimize;
+
+    /* == Retrieve the vertices to remove == */
+    for (auto *vertex : graph->vertices()) {
+        if (vertex->type() == PiSDFVertexType::INIT) {
+            auto *sink = vertex->outputEdge(0)->sink();
+            if (sink->type() == PiSDFVertexType::END) {
+                verticesToOptimize.push_back(vertex);
+            }
+        }
+    }
+
+    /* == Remove useless init / end connections == */
+    for (auto *init : verticesToOptimize) {
+        auto *edge = init->outputEdge(0);
+        auto *end = edge->sink();
+        graph->removeEdge(edge);
+        Spider::Logger::printVerbose(LOG_OPTIMS, "InitEndOptimizer: removing init [%s] and end [%s] vertices.\n",
+                                     init->name().c_str(), end->name().c_str());
+        graph->removeVertex(init);
+        graph->removeVertex(end);
+    }
+    return graph;
 }
 
 #endif //SPIDER2_PISDFINITENDOPTIMIZER_H
