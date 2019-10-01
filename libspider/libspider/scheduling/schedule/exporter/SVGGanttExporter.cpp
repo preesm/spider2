6d26fec4b9232cb3244c50d182249d3475c1c4e5
Arrestier Florian <florian.arrestier@insa-rennes.fr>
2019-10-01 23:50:42 +0200

    (EXPORTER) adding svg exporter for gantt.

---
+++ libspider/libspider/scheduling/schedule/exporter/SVGGanttExporter.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright or © or Copr. IETR/INSA - Rennes (2013 - 2019) :
+ *
+ * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2018)
+ * Clément Guy <clement.guy@insa-rennes.fr> (2014)
+ * Florian Arrestier <florian.arrestier@insa-rennes.fr> (2017-2019)
+ * Hugo Miomandre <hugo.miomandre@insa-rennes.fr> (2017)
+ * Julien Heulot <julien.heulot@insa-rennes.fr> (2013 - 2015)
+ * Yaset Oliva <yaset.oliva@insa-rennes.fr> (2013 - 2014)
+ *
+ * Spider is a dataflow based runtime used to execute dynamic PiSDF
+ * applications. The Preesm tool may be used to design PiSDF applications.
+ *
+ * This software is governed by the CeCILL  license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
+/* === Include(s) === */
+
+#include <scheduling/schedule/exporter/SVGGanttExporter.h>
+#include <spider-api/archi.h>
+#include <spider-api/pisdf.h>
+#include <archi/Platform.h>
+#include <archi/ProcessingElement.h>
+#include <graphs/pisdf/PiSDFVertex.h>
+#include <graphs/pisdf/PiSDFGraph.h>
+#include <scheduling/schedule/Schedule.h>
+#include <scheduling/schedule/ScheduleJob.h>
+#include <iomanip>
+
+/* === Static variable(s) === */
+
+/* === Static function(s) === */
+
+/* === Method(s) implementation === */
+
+void Spider::SVGGanttExporter::print() const {
+    print("./gantt.svg");
+}
+
+void Spider::SVGGanttExporter::print(const std::string &path) const {
+    std::ofstream file{path, std::ios::out};
+    print(file);
+
+    /* == We should not do this manually but this will ensure that data are correctly written even if it crashes == */
+    file.close();
+}
+
+void Spider::SVGGanttExporter::print(std::ofstream &file) const {
+
+}
---
+++ libspider/libspider/scheduling/schedule/exporter/SVGGanttExporter.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright or © or Copr. IETR/INSA - Rennes (2013 - 2019) :
+ *
+ * Antoine Morvan <antoine.morvan@insa-rennes.fr> (2018)
+ * Clément Guy <clement.guy@insa-rennes.fr> (2014)
+ * Florian Arrestier <florian.arrestier@insa-rennes.fr> (2017-2019)
+ * Hugo Miomandre <hugo.miomandre@insa-rennes.fr> (2017)
+ * Julien Heulot <julien.heulot@insa-rennes.fr> (2013 - 2015)
+ * Yaset Oliva <yaset.oliva@insa-rennes.fr> (2013 - 2014)
+ *
+ * Spider is a dataflow based runtime used to execute dynamic PiSDF
+ * applications. The Preesm tool may be used to design PiSDF applications.
+ *
+ * This software is governed by the CeCILL  license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+#ifndef SPIDER2_SVGGANTTEXPORTER_H
+#define SPIDER2_SVGGANTTEXPORTER_H
+
+/* === Include(s) === */
+
+#include <common/Exporter.h>
+
+namespace Spider {
+
+    /* === Forward declaration(s) === */
+
+    class Schedule;
+
+    class ScheduleJob;
+
+    /* === Class definition === */
+
+    class SVGGanttExporter : public Exporter {
+    public:
+
+        explicit SVGGanttExporter(const Schedule *schedule) : Exporter(), schedule_{schedule} { }
+
+        ~SVGGanttExporter() override = default;
+
+        /* === Method(s) === */
+
+        /**
+         * @brief Print the graph to default file path.
+         * @remark default path: ./gantt.svg
+         */
+        void print() const override;
+
+        void print(const std::string &path) const override;
+
+        void print(std::ofstream &file) const override;
+
+    private:
+        const Schedule *schedule_ = nullptr;
+
+        /* === Private method(s) === */
+
+        void headerPrinter(std::ofstream &file) const;
+        
+        void axisPrinter(std::ofstream &file) const;
+
+        void jobPrinter(std::ofstream &file, const ScheduleJob &job) const;
+    };
+
+    /* === Inline method(s) === */
+}
+#endif //SPIDER2_SVGGANTTEXPORTER_H
