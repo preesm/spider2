9fa5d5b81a0e8270059a7ec36637c95573f89f9b
Florian Arrestier <florian.arrestier@insa-rennes.fr>
2020-05-08 13:03:36 +0200

    (REFACTO) removed AbstractVertex.

--- libspider/graphs/pisdf/Vertex.h
+++ libspider/graphs/pisdf/Vertex.h
@@ -37,12 +37,7 @@
 
 /* === Include(s) === */
 
-#include <cstdint>
-#include <string>
 #include <containers/array.h>
-#include <graphs/abstract/AbstractVertex.h>
-#include <graphs-tools/helper/visitors/PiSDFVisitor.h>
-#include <graphs/pisdf/Edge.h>
 #include <runtime/common/RTInfo.h>
 
 namespace spider {
@@ -53,9 +48,13 @@ namespace spider {
 
         class Graph;
 
+        class Edge;
+
+        class Visitor;
+
         /* === Class definition === */
 
-        class Vertex : public AbstractVertex<pisdf::Graph, pisdf::Edge> {
+        class Vertex {
         public:
 
             explicit Vertex(VertexType type, std::string name, size_t edgeINCount = 0, size_t edgeOUTCount = 0);
@@ -70,9 +69,41 @@ namespace spider {
 
             Vertex &operator=(const Vertex &) = delete;
 
-            ~Vertex() noexcept override;
+            virtual ~Vertex() noexcept;
 
             /* === Method(s) === */
+
+            /**
+             * @brief Connect an input edge at given position.
+             * @param edge  Pointer to the edge to connect.
+             * @param pos   Input position where to connect the edge.
+             * @throw @refitem std::out_of_range.
+             * @throw @refitem spider::Exception if an edge already exists at this position.
+             */
+            virtual void connectInputEdge(Edge *edge, size_t pos);
+
+            /**
+             * @brief Connect an output edge at given position.
+             * @param edge  Pointer to the edge to connect.
+             * @param pos   Output position where to connect the edge.
+             * @throw @refitem std::out_of_range.
+             * @throw @refitem spider::Exception if an edge already exists at this position.
+             */
+            virtual void connectOutputEdge(Edge *edge, size_t pos);
+
+            /**
+             * @brief Disconnect input edge on port ix. If no edge is connected, nothing happens.
+             * @remark Call @refitem Edge::setSink to reset the edge if found.
+             * @param ix  Index of the input edge to disconnect.
+             */
+            virtual Edge *disconnectInputEdge(size_t ix);
+
+            /**
+             * @brief Disconnect output edge on port ix. If no edge is connected, nothing happens
+             * @remark Call @refitem Edge::setSource to reset the edge if found.
+             * @param ix  Index of the output edge to disconnect.
+             */
+            virtual Edge *disconnectOutputEdge(size_t ix);
 
             /**
              * @brief Generic method that accept a visitor for class specific treatments.
@@ -134,6 +165,64 @@ namespace spider {
             inline const T *convertTo() const { return static_cast<const T *>(this); }
 
             /* === Getter(s) === */
+
+            /**
+             * @brief Get the name string of the vertex.
+             * @return name of the vertex.
+             */
+            inline const std::string &name() const { return name_; };
+
+            /**
+             * @brief Get the ix of the vertex in the containing graph.
+             * @return ix of the vertex (SIZE_MAX if no ix).
+             */
+            inline const size_t &ix() const { return ix_; };
+
+            /**
+             * @brief Returns the graph of the vertex (if any)
+             * @return Pointer to the containing graph, nullptr else.
+             */
+            inline Graph *graph() const { return graph_; }
+
+            /**
+             * @brief A const reference on the array of input edges. Useful for iterating on the edges.
+             * @return const reference to input edge array
+             */
+            inline const vector<Edge *> &inputEdgeVector() const { return inputEdgeVector_; };
+
+            /**
+             * @brief Get input edge connected to port Ix.
+             * @param ix Index of the input port.
+             * @return @refitem spider::pisdf::Edge
+             * @throw std::out_of_range.
+             */
+            inline Edge *inputEdge(size_t ix) const { return inputEdgeVector_.at(ix); };
+
+            /**
+             * @brief Get the number of input edges connected to the vertex.
+             * @return number of input edges.
+             */
+            inline size_t inputEdgeCount() const { return inputEdgeVector_.size(); };
+
+            /**
+             * @brief A const reference on the array of output edges. Useful for iterating on the edges.
+             * @return const reference to output edge array.
+             */
+            inline const vector<Edge *> &outputEdgeVector() const { return outputEdgeVector_; };
+
+            /**
+             * @brief Get input edge connected to port Ix.
+             * @param ix Index of the output port.
+             * @return @refitem spider::pisdf::Edge
+             * @throw std::out_of_range.
+             */
+            inline Edge *outputEdge(size_t ix) const { return outputEdgeVector_.at(ix); };
+
+            /**
+             * @brief Get the number of output edges connected to the vertex.
+             * @return number of output edges.
+             */
+            inline size_t outputEdgeCount() const { return outputEdgeVector_.size(); };
 
             /**
              * @brief Get the subtype of the vertex.
@@ -241,15 +330,42 @@ namespace spider {
              */
             void setInstanceValue(size_t value);
 
+            /**
+             * @brief Set the name of the vertex.
+             * @remark This method will replace current name of the vertex.
+             * @warning No check on the name is performed to see if it is unique in the graph.
+             * @param name  Name to set.
+             */
+            virtual inline void setName(std::string name) { name_ = std::move(name); };
+
+            /**
+             * @brief Set the ix of the vertex in the containing graph.
+             * @param ix Ix to set.
+             */
+            virtual inline void setIx(size_t ix) { ix_ = ix; };
+
+            /**
+             * @brief Set the containing graph of the vertex.
+             * @remark override current value.
+             * @remark if graph is nullptr, nothing happens.
+             * @param graph  Pointer to the graph to set.
+             */
+            virtual void setGraph(Graph *graph);
+
         protected:
+            vector<Edge *> inputEdgeVector_;                       /* = Vector of input Edge = */
+            vector<Edge *> outputEdgeVector_;                      /* = Vector of output Edge = */
             vector<std::shared_ptr<Param>> inputParamVector_;      /* = Vector of input Params = */
             vector<std::shared_ptr<Param>> refinementParamVector_; /* = Vector of refinement Params = */
             vector<std::shared_ptr<Param>> outputParamVector_;     /* = Vector of output Params = */
+            std::string name_ = "unnamed-vertex";                  /* = Name of the Vertex (uniqueness is not required) = */
             std::shared_ptr<RTInfo> rtInformation_;                /* = Runtime information of the Vertex (timing, mappable, etc.) = */
             const Vertex *reference_ = this;   /* =
                                                 * Pointer to the reference Vertex.
                                                 * Default is this, in case of copy, point to the original Vertex.
                                                 * = */
+            Graph *graph_ = nullptr;           /* = Graph of the vertex = */
+            size_t ix_ = SIZE_MAX;             /* = Index of the Vertex in the containing Graph = */
             size_t scheduleJobIx_ = SIZE_MAX;  /* =
                                                 * Index of the schedule job associated to this Vertex.
                                                 * Needed in case of deletion of vertex between successive.
@@ -264,6 +380,25 @@ namespace spider {
              * @brief Verify that VertexType and vertex properties are coherent.
              */
             void checkTypeConsistency() const;
+
+        private:
+
+            /**
+             * @brief Disconnect an edge from the given edge vector (input or output).
+             * @param edges  Vector of edges (input or output).
+             * @param ix     Index of the edge to disconnect.
+             * @return pointer to the disconnected edge, nullptr else.
+             */
+            Edge *disconnectEdge(vector<Edge *> &edges, size_t ix);
+
+            /**
+             * @brief Connect an edge in the given edge vector (input or output).
+             * @param edges  Vector of edges (input or output).
+             * @param edge   Pointer to the edge to be connected.
+             * @param ix     Index of the edge to connect.
+             * @throw spider::Exception if edge already exists in the vector at given index.
+             */
+            void connectEdge(vector<Edge *> &edges, Edge *edge, size_t ix);
         };
     }
 }
